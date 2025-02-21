912876df496568a1b9f535d89b2575d36b8cdc14
Frank Mwito <Mfranklyne039@gmail.com>
2025-02-21 12:18:55 +0300

    Locust Tags

---
+++ locustfile_test1.py
@@ -0,0 +1,19 @@
+from locust import HttpUser, task, between, tag
+
+class MyUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task(3)
+    @tag('home')
+    def load_homepage(self):
+        self.client.get("/")
+
+    @task(2)
+    @tag('shows')
+    def load_shows(self):
+        self.client.get("/shows/")
+
+    @task(1)
+    @tag('about')
+    def load_blog(self):
+        self.client.get("/about/")
