dce8c24e717508357681588781a11c8cd182b5b8
Frank Mwito <Mfranklyne039@gmail.com>
2025-03-06 18:54:16 +0300

    setup and teardown performance testing for google search request

--- src/test.py
+++ src/test.py
@@ -1,23 +1,26 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, events, stats
 from datetime import datetime
 
+host = "https://www.google.com"
+# Test start Hook
+@events.test_start.add_listener
+def on_test_start(environment, **kwargs):
+    print("Test is Starting! Perform setup.....")
+    # perform setup here
+    print("Environment host:", environment.host)
+    
+# Test stop Hook
+@events.test_stop.add_listener
+def on_test_stop(environment, **kwargs):
+    print("Test is Stopping! Perform cleanup.....")
+    # perform cleanup here
+    print("Total requests:", environment.stats.total.num_requests)
 class MywebUser(HttpUser):
     wait_time = between(1, 2)
-   # host = "https://www.google.com"
-    
-    
-    @task(3)
+    # host = "https://www.google.com"
+   
+    @task
     def web(self):
+        self.client.get("/search?q=locust")
         print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         print("Hello, frank! am a web user")
-        
-class MymobileUser(HttpUser):
-    wait_time = between(1, 2)
-   # host = "https://www.google.com"
-    
-    
-    @task(2)
-    def mobile(self):
-        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        print("Hello, frank! am a mobile user")
-        
