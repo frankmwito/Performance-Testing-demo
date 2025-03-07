f1c043851c064fbc54ba0fca5ebfe2c93b73aba4
Frank Mwito <Mfranklyne039@gmail.com>
2025-03-07 11:57:34 +0300

    Performance tests with csv reports

--- src/test1.py
+++ src/test1.py
@@ -1,14 +1,15 @@
 
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, TaskSet
 
-def web(self):
-    print("am a web user")
-    
-def mobile(self):
-    print("am a mobile")
+
+class MyTaskSet(TaskSet):
+    def web(self):
+        print("am a web user")
+    def mobile(self):
+        print("am a mobile")
 
 
 class MywebUser(HttpUser):
     wait_time = between(1, 2)
     
-    tasks = {web: 2, mobile: 1 }
+    tasks = [MyTaskSet]
